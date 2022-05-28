60cc3c78924bfe2423506a4be473c7702fb71354
Stephan Wendel <me@stephanwe.de>
2022-05-28 17:12:55 +0200

    func change_service_user n.12
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- user-rename.sh
+++ user-rename.sh
@@ -40,7 +40,7 @@ function change_www_root {
 function change_service_user {
     ### Filter nginx service first!
     local -a servicefile
-    servicefile=("${SERVICES[@]/nginx}")
+    servicefile=( "${SERVICES[@]/nginx}" )
 
     for i in "${servicefile[@]}"; do
         sudo -E sed -i 's/pi/'"${DEFAULT_USER}"'/g' "${SYSTEMD_DIR}/${i}.service"
